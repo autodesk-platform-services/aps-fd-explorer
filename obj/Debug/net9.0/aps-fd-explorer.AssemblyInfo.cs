b718eb17ae389db4ae31d1f06b7eeee25aa18b4a
Denis Grigor <apprentice3d@gmail.com>
2025-07-16 14:31:10 -0400

    fixed the project name

--- obj/Debug/net9.0/aps-fd-explorer.AssemblyInfo.cs
+++ obj/Debug/net9.0/aps-fd-explorer.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("aps-fd-explorer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+186aaf8d74a18356d8eb597183e4eebee9367531")]
 [assembly: System.Reflection.AssemblyProductAttribute("aps-fd-explorer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("aps-fd-explorer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
